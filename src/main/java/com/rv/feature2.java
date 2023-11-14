ac92d6829c7b92f0f925d8777ce35491a5fb8440
Ravi Mergu <ravimergu1@gmail.com>
2023-11-14 22:57:26 +0530

    f-2 c-2

--- src/main/java/com/rv/feature2.java
+++ src/main/java/com/rv/feature2.java
@@ -3,6 +3,7 @@ package com.rv;
 public class feature2 {
     public static void main(String[] args) {
         System.out.println("feature2 commit1");
+        System.out.println("feature2 commit2");
     }
 
 }
